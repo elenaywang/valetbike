e3b7dcab65c3a26e621abae10fc09ff3ad826f63
Lesly-Gonzalez <lgonzalezherrera@smith.com>
2023-11-04 14:58:32 -0400

    Added URLs for pricing, sign up, and log in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root to: "stations#index"
+
+  #Adding route for the pricing page
+  match 'pricing', to: "stations#pricing", via: :get
+
+  #Adding route for the sign up page. For now I'll use the stations_controller. What are we using the application_controller for? Should I create another controller?
+  match 'sign_up', to: "stations#sign_up", via: :get
+
+  #Adding route for the log in page. For now I'll use the stations_controller.
+  match 'log_in', to: "stations#log_in", via: :get
+  
 end
