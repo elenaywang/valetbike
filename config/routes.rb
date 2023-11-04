4ada86d268cc38325ec41fb3351afa7f0a915603
Lesly-Gonzalez <lgonzalezherrera@smith.com>
2023-11-04 15:34:35 -0400

    Created URLs for Explore page and Help page

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,17 @@ Rails.application.routes.draw do
   #Adding route for the pricing page
   match 'pricing', to: "stations#pricing", via: :get
 
-  #Adding route for the sign up page. For now I'll use the stations_controller. What are we using the application_controller for? Should I create another controller?
+  #Adding route for the sign up page.
   match 'sign_up', to: "stations#sign_up", via: :get
 
-  #Adding route for the log in page. For now I'll use the stations_controller.
+  #Adding route for the log in page. 
   match 'log_in', to: "stations#log_in", via: :get
-  
+
+  #Adding route for the help page
+  match 'help', to: "stations#help", via: :get
+
+  #Adding route for the explore page
+  match 'explore', to: "stations#explore", via: :get
+
+
 end
