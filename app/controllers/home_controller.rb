d9b53bacd4f8c5a581c0127d13f52937b261cc7b
yloo123 <yinglam.loo@outlook.com>
2023-11-06 16:21:13 -0500

    Created map action that accesses station data

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+
+  def index
+
+  end
+  
+  def map
+    if params[:station].present?
+      @stations = Station.near(params[:station])
+  else
+      @stations = Station.all.order(identifier: :asc)
+  end
+  end
+
+
+end
