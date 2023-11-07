81701bb62db540f0e8b88e06861a7922e899386f
yloo123 <yinglam.loo@outlook.com>
2023-11-06 20:13:40 -0500

    Created routes towards the map page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  root to: "stations#index"
+
+  # root to: "home#index"
+  root to: "home#index"
+
+  match 'map', to: "home#map", via: :get
+
+  # resources:bikes, only: [:map]
+  # resources:stations, only: [:map]
+  
+  get 'home/index'
+  get 'home/map'
+  get 'bikes/index'
+  get 'bikes/_row'
+  
 end
