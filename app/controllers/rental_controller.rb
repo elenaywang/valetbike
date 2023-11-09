636ef951e073cdb7acc9848702187d37a4da0e2b
mgilliom <mgilliom@smith.edu>
2023-11-09 14:45:43 -0500

    click 'rent bike from here' from the root page, takes you to rental: new view where you can put in a time. no submit button,and it's just the new crud, not the create crud.

--- app/controllers/rental_controller.rb
+++ app/controllers/rental_controller.rb
@@ -1,7 +1,9 @@
 class RentalController < ApplicationController
   def new
-    @stations = Station.all
-    @task = Task.new
+    @stations = Station.all #eventaully change to...the chosen station?
+    @rental = Rental.new
+    @time = DateTime.now
+    #sth like this too @user = User.loggedIn
   end
 
   def edit
