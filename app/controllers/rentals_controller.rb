9ac0a9c4cd6a8bb0f2967932c79e6d5fb2cbaa79
Lesly-Gonzalez <lgonzalezherrera@smith.com>
2023-11-13 22:27:37 -0500

    updating master

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,43 @@
+class RentalsController < ApplicationController
+  def index
+    @rentals = Rental.order(:checkout)
+  end
+  
+  def new
+    #@stations = Station.all #eventaully change to...the chosen station?
+    @rental = Rental.new
+    #@time = DateTime.now
+    #sth like this too @user = User.loggedIn
+  end
+ 
+  def create
+    @rental = Rental.new(rental_params)
+    #Rental.code
+    @random_number = "%07d" % rand(10000000)
+    @rental.number = @random_number
+    if @rental.save
+      redirect_to rentals_path 
+    else 
+      #eventually need to tell you to fix error
+      render('new')
+    end
+  end
+
+  def edit
+    @rental = Rental.find(params[:id])
+  end
+
+  def update
+    @rental = Rental.find(params[:id])
+    if @rental.update(rental_params)
+      redirect_to rental_path(@rental)
+    else
+      render('edit')
+    end
+  end
+
+  private
+  def rental_params
+    params.require(:rental).permit(:checkout, :bike_id)
+  end
+end
