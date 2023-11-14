863433a8e8a2adb96d63d0c66bd030b47f345645
Lesly-Gonzalez <lgonzalezherrera@smith.com>
2023-11-13 21:49:56 -0500

    testing master

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,5 +3,20 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all.order(identifier: :asc)
   end
+
+  def pricing
+  end
+
+  def sign_up
+  end
+ 
+  def log_in
+  end
+
+  def help
+  end
+
+  def explore
+  end
   
 end
