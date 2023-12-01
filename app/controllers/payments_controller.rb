a74148730ee3a8e26a29d4475664eb183481aa04
Chris Dong <cdong02@gmail.com>
2023-12-01 01:10:37 -0500

    Updates user's payment_id column when payment info is saved

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,25 +1,31 @@
 class PaymentsController < ApplicationController
-    def index
-        @payment = Payment.order(:first_name)
-    end
+    # def index
+    #     @payment = Payment.order(:first_name)
+    # end
 
     def show
-        @user = User.find(params[:id])
-        @payment = @user.payment
+        #@user = current_user
+        @payment = Payment.find_by(user_id: current_user.id)
     end
 
     def new
-        @user = User.find(params[:user_id])
+        @user = current_user
         @payment = Payment.new
     end
 
     def create
-       @payment = Payment.new(payment_params) 
-       @payment.user_id = params[:user_id]
-        if @payment.save
-            redirect_to user_payments_path
+       if current_user.payment_id.blank?
+        @payment = Payment.new(payment_params) 
+        @payment.user_id = params[:user_id]
+            if @payment.save
+                puts @payment.id
+                current_user.update_column(:payment_id, @payment.id)
+                redirect_to user_payments_path
+            else
+                render('new')
+            end
         else
-            render('new')
+            redirect_to user_payments_path
         end
     end
 
@@ -44,4 +50,4 @@ end
 # #     end
 # #   end
 
-# end
+## end
