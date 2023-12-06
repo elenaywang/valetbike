38d964634434584cf22ab3a88d4eec3a8330a1e3
Chris Dong <cdong02@gmail.com>
2023-12-06 18:26:37 -0500

    Redirects users if they attempt to access payment info of other user ids

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,7 @@
 class PaymentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :check_user, only: [:new, :show, :edit]
+    before_action :check_payment, only: [:show, :edit]
     #validators for form input here
 
     # def index
@@ -12,32 +14,34 @@ class PaymentsController < ApplicationController
     end
 
     def new
-        @user = current_user
-        @payment = Payment.new
+        if current_user.payment_id.blank?
+            @user = current_user
+            @payment = Payment.new
+        else
+            redirect_to edit_user_payment_path(id: current_user.payment_id)
+        end
+
     end
 
     def create
-        puts "WTFF"
-        if current_user.id == params[:user_id]
-            if current_user.payment_id.blank? 
-                @payment = Payment.new(payment_params) 
-                @payment.user_id = params[:user_id]
-                #@payment.user gets user
-                    if @payment.save
-                        flash.notice = "Payment information saved"
-                        current_user.update_column(:payment_id, @payment.id)
-                        #current user.payment = payment
-                        redirect_to user_payments_path
-                    else
-                        flash.alert = "Unable to save payment information"
-                        render('new')
-                    end
-            else
-                redirect_to user_payments_path
-            end
+        if current_user.payment_id.blank? 
+            @payment = Payment.new(payment_params) 
+            @payment.user_id = params[:user_id]
+            #@payment.user gets user
+                if @payment.save                        flash.notice = "Payment information saved"
+                    current_user.update_column(:payment_id, @payment.id)
+                    #current user.payment = payment
+                    redirect_to user_payments_path
+                else
+                    flash.alert = "Unable to save payment information"
+                    render('new')
+                end
         else
-            redirect_to user_payments_path
+            redirect_to user_payment_path
         end
+        # else
+        #     redirect_to user_payments_path(current_user)
+        # end
     end
 
     def edit
@@ -46,6 +50,7 @@ class PaymentsController < ApplicationController
     end
 
     def update
+        @user = current_user
         @payment = Payment.find_by(user_id: current_user.id)
         if @payment.update(payment_params)
             flash.notice = "Payment information updated"
@@ -53,6 +58,23 @@ class PaymentsController < ApplicationController
         else
             flash.alert = "Unable to update payment information"
             render ('edit')
+        end
+    end
+
+    def check_user
+        # Rails.logger.debug "current_user.id: #{current_user.id}"
+        # Rails.logger.debug "params[:user_id]: #{params[:user_id]}"
+        if current_user.id != params[:user_id].to_i
+            #flash.alert = "Access prohibited"
+            redirect_to user_payments_path(current_user)
+            #change this redirect to take you back to profile editing page
+        end
+    end
+    
+    def check_payment
+        if current_user.payment_id != params[:id].to_i
+            redirect_to user_payments_path(current_user)
+            #change redirect to take you back to profile
         end
     end
 
